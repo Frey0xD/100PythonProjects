353d034b7890336ccf883fda436d7d31537c5333
Frey <Frey0xd@gmail.com>
2023-03-14 04:57:26 +0530

    0002 && 0003

---
+++ 0002.variable.py
@@ -0,0 +1,15 @@
+
+#name = input("What is your name:- \n")
+#print("Your name is" + " " + name)
+
+# One more thing i want to show u here is 
+
+# The value of variables
+
+name = "Sahu"
+print(name)
+
+print("now it will be vim from sahu")
+name  = "VIM"
+print(name)
+print(len(name))
---
+++ 0003.Var_Exercise.py
@@ -0,0 +1,18 @@
+# Write a program that switch the value stored in the variable a and b and do that without changing anything which is pre written 
+
+
+# Pre
+
+a = input("a:")
+b = input("b:")
+
+######## OUR CODE #########
+
+c = a
+a = b
+b = c
+
+###########################
+
+print("a = " + a)
+print("b = " + b)
